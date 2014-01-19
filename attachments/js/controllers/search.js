0cfa54862e0f57673f07219b79ce841156baf8e1
Josh Bavari <josh@raisemore.com>
2014-01-19 04:22:21 -0600

    Added some dummy data to the search controller - allows the search page to render the plugin results. Basic functionality is there

--- attachments/js/controllers/search.js
+++ attachments/js/controllers/search.js
@@ -1,6 +1,16 @@
 angular.module('registry.controllers').controller('SearchController', ['$rootScope', '$routeParams', '$scope', '$location', '$http', 'SearchService', function($rootScope, $routeParams, $scope, $location, $http, SearchService) {
     
+    var results = [];
+
     $scope.searchText = $routeParams.searchText || SearchService.searchText || '';
+
+    $scope.searchResults = [{
+    	package_id: 'org.apache.cordova.device', 
+    	name: 'Device',
+    	description: 'Pulls device information',
+    	version: '0.5.1',
+    	downloads: 25617
+    }];
 
     $scope.searchPlugins = function() {
         $location.path('/search/' + $scope.searchText);
