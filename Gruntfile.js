0f2b629a5e818bc6f322d792d722f46e906dc1e4
Steven Gill <stevengill97@gmail.com>
2014-04-02 15:49:44 -0700

    updated some styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -89,6 +89,7 @@ module.exports = function(grunt) {
       grunt.task.run([
           'configureProxies:localRegistry',
           'connect:develop',
+          'shell:reloadServer',
           'watch'
       ]);
   });
