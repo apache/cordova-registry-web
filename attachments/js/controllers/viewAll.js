87b33e7f54485e6ab3c7ed7eac9058e818975fd6
Raymond Camden <raymondcamden@gmail.com>
2014-11-05 14:04:06 -0600

    Add filter by platform to add plugin view

--- attachments/js/controllers/viewAll.js
+++ attachments/js/controllers/viewAll.js
@@ -2,6 +2,60 @@ angular.module('registry.controllers').controller('ViewAllController', ['$rootSc
     
     $scope.loading = true;    
 
+	$scope.showFilters = false;
+	$scope.hideFilters = true;
+
+	$scope.filteriOS = false;
+	$scope.filterAndroid = false;
+	$scope.filterWindowsPhone7 = false;
+	$scope.filterWindowsPhone8 = false;
+	$scope.filterWindows8 = false;
+	$scope.filterFirefox = false;
+	$scope.filterBlackberry = false;
+	$scope.filterUbuntu = false;
+	$scope.filterBrowser = false;
+
+	$scope.selectedPlatforms = function() {
+		//send back null if we arent showing filters
+		if(!$scope.showFilters) {
+			return null;
+		}
+
+		var platformFilters = [];
+		if($scope.filterFirefox) {
+			platformFilters.push('firefoxos');
+		}
+		if($scope.filterAndroid) {
+			platformFilters.push('android');
+		}
+		if($scope.filterFireOs) {
+			platformFilters.push('amazon-fireos');
+		}
+		if($scope.filterUbuntu) {
+			platformFilters.push('ubuntu');
+		}
+		if($scope.filteriOS) {
+			platformFilters.push('ios');
+		}
+		if($scope.filterBlackberry) {
+			platformFilters.push('blackberry10');
+		}
+		if($scope.filterWindowsPhone7) {
+			platformFilters.push('wp7');
+		}
+		if($scope.filterWindowsPhone8) {
+			platformFilters.push('wp8');
+		}
+		if($scope.filterWindows8) {
+			platformFilters.push('windows8');
+		}
+		if($scope.filterBrowser) {
+			platformFilters.push('browser');
+		}
+
+		return platformFilters;
+	};
+	
     $scope.getPlugins = function(){
         $http.get('/_view/searcher/').
                 success(function(data, status, headers, config) {
@@ -29,4 +83,10 @@ angular.module('registry.controllers').controller('ViewAllController', ['$rootSc
 
     }
     $scope.getPlugins();
+	
+	$scope.toggleFilters = function toggleFilters() {
+		$scope.hideFilters = $scope.showFilters;
+		$scope.showFilters = !($scope.showFilters);
+	};
+	
 }]);
